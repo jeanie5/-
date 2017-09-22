3915b43929cd4b02fc109dba0b7a821c02f9b341
jeanie5@naver.com <CBNU@CBNU-PC>
2017-09-22 15:39:15 +0900

    https://github.com/jeanie5/-.git

---
+++ eclipse-workspace/개발프로젝트/src/개발프로젝트/제어문1.java
@@ -0,0 +1,14 @@
+package 개발프로젝트;
+
+public class 제어문1
+{
+	public static void main(String args[]) 
+	{
+		int jumsu = 80;  
+		if(jumsu >= 60) 
+		{
+			System.out.println("당신은 합격입니다");
+		}
+		System.out.println("당신의 점수는" + jumsu + "입니다" );
+	}
+}
